6ae8c23e39963b927adcbd9e117d01bd519d953f
Lowe Berg <loweberg04@gmail.com>
2025-09-18 11:24:36 +0200

    organized code

--- Task1.py
+++ Task1.py
@@ -6,17 +6,28 @@ def f1(x, y):  # guess limit is 0.5
     return (x**2-x*y)/(x**2-y**2)
 
 
-res = 50
-a, b = 1, 1
-np.vectorize(f1)
-X = np.outer(np.linspace(0, 2, res), np.ones(res))
-Y = X.copy().T
-Z = (f1(X, Y))
+def f2(x, y):
+    return (x**2 + y**2)/(x**2 + x*y + y**2)
 
+
+def gen_z(f, a, b, res):
+    X_, Y_ = np.meshgrid(np.linspace(a - 1, a + 1, res), np.linspace(b - 1, b + 1, res))
+
+    Z_ = (f(X_, Y_))
+
+    return X_, Y_, Z_
+
+
+# setup
 fig = plt.figure()
 ax = plt.axes(projection='3d')
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+
+
+X, Y, Z = gen_z(f2, 0, 0, 50)
 
 ax.plot_surface(X, Y, Z)
 
-plt.grid()
 plt.show()
