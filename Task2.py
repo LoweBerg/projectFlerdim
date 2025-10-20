f0c323b424141ff36b48fbcecc5677ed24bfc523
Lowe Berg <loweberg04@gmail.com>
2025-10-20 12:00:42 +0200

    finished task 2

--- Task2.py
+++ Task2.py
@@ -15,6 +15,14 @@ def grad(f, x, y, h):  # returns the gradient of a function f at a point x, y by
 
 
 def hessian(f, x, y, h):
+    """
+    Calculates the numerical hessian of a generic function around a point (x, y)
+    :param f: function to be analyzed
+    :param x: x-value of point
+    :param y: y-value of point
+    :param h: small value to approximate derivative
+    :return: hessian matrix for functio
+    """
     dfxx = (ddx(f, x+h, y, h) - ddx(f, x, y, h))/h
     dfxy = (ddx(f, x, y+h, h) - ddx(f, x, y, h))/h
     # not necessary but can be nice to have anyway
@@ -25,26 +33,31 @@ def hessian(f, x, y, h):
 
 
 def func(x, y):
+    """
+    function from assignment
+    """
     return np.sin(x+y)
 
 
 def analytic_grad(x, y):
+    """
+    calculates the analytical gradient of sin(x+y) at a point (x, y)
+    """
     return np.array([np.cos(x+y), np.cos(x+y)])
 
 
 def analytic_hess(x, y):
+    """
+    calculates the analytical hessian of sin(x+y) at a point (x, y)
+    """
     return np.array([[-np.sin(x+y), -np.sin(x+y)], [-np.sin(x+y), -np.sin(x+y)]])
 
 
-def gen_z(f, x, y, width, res):
-    X_, Y_ = np.meshgrid(np.linspace(x - width, x + width, res), np.linspace(y - width, y + width, res))
-
-    Z_ = (f(X_, Y_))
-
-    return X_, Y_, Z_
-
-
 def gen_err(y1_, y2_):
+    """
+    generates an array of absolute error between two vector arrays
+    by comparing distance between vectors
+    """
     diff = np.array([y2_[0] - y1_[0], y2_[1] - y1_[1]])
     err = np.array([np.sqrt(diff[0]**2+diff[1]**2)])
 
@@ -52,7 +65,11 @@ def gen_err(y1_, y2_):
 
 
 def f5(x, y):  # (0, +-1.25)
+    """
+    Second function from Eq. 2
+    """
     return (x**2+3*y**2)*np.e**(-x**2-y**2)
+
 
 # switches
 plot1 = 0
@@ -102,13 +119,16 @@ if plot2:
     plt.plot(X2, Y2)
 
 X3, Y3 = np.meshgrid(np.linspace(-100, 100, 500), np.linspace(-100, 100, 500))
-Z1 = hessian(func, X3, Y3, 10**-3)[0, 0]
+Z1 = hessian(func, X3, Y3, 10**-6)[0, 0]
 Z2 = analytic_hess(X3, Y3)[0, 0]
 
 if plot3:
     fig = plt.figure()
     plt.title("Error in first value of hessian matrix")
     ax = fig.add_subplot(projection='3d')
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("error")
     ax.plot_surface(X3, Y3, Z2-Z1)
 
 
