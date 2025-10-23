56dc1296fdc4987e785e95f63ec31aab48073fe7
Lowe Berg <loweberg04@gmail.com>
2025-10-23 14:33:26 +0200

    löste buggar i task 3

--- Frejas freaky combination.py
+++ Frejas freaky combination.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
-from math import e
-from math import floor
+from math import e, floor, sqrt
 
 X = np.linspace(-10**(-5), 10**(-5), 51)
 Y = np.linspace(-10**(-5), 10**(-5), 51)
@@ -15,29 +14,34 @@ for i in range(np.size(X)):
 Mid = floor(51/2)
 H = (2*10**-5)/51
 
-DDX = (roots[Mid+1,Mid]-roots[Mid,Mid])/H
-DDY = (roots[Mid,Mid+1]-roots[Mid,Mid])/H
-DFXX = (((roots[Mid+2,Mid]-roots[Mid+1,Mid])/H) - DDX)/H
-DFXY = (((roots[Mid+1,Mid+1]-roots[Mid+1,Mid])/H) - DDX)/H
-DFYY = (((roots[Mid,Mid+2]-roots[Mid,Mid+1])/H) - DDY)/H
+DDX = (roots[Mid+1, Mid]-roots[Mid, Mid])/H
+DDY = (roots[Mid, Mid+1]-roots[Mid, Mid])/H
+DFXX = (roots[Mid+2, Mid] - 2*roots[Mid+1, Mid] + roots[Mid, Mid])/(H**2)
+DFXY = (roots[Mid+1, Mid+1]-roots[Mid, Mid+1]-roots[Mid+1, Mid]-roots[Mid, Mid])/(H**2)
+DFYY = (roots[Mid, Mid+2] - 2*roots[Mid, Mid+1] + roots[Mid, Mid])/(H**2)
+
 
 def P_2(x, y):
-    return roots[Mid,Mid] + DDX*x + DDY*y + (DFXX*x**2 + 2*DFXY*x*y + DFYY*y**2)/2
+    return roots[Mid, Mid] + DDX*x + DDY*y + (DFXX*x**2 + 2*DFXY*x*y + DFYY*y**2)/2
 
-print("wa")
+
+print(roots[Mid+1, Mid+1])
+print(roots[Mid+1, Mid])
 print(DDX)
-print(DDY)
-print(DFXX)
 print(DFXY)
-print(DFYY)
+
 
 #Surface plot
 
-"""
-X_, Y_ = np.meshgrid(np.linspace(-5, 5, 50), np.linspace(-5, 5, 50))
+X = np.linspace(-1, 1, 51)
+Y = np.linspace(-1, 1, 51)
+roots = np.zeros((51, 51))#loop that iterates over every possible combination on the lists X and Y
 
-"""
-X_, Y_ = np.meshgrid(np.linspace(-10**(-5), 10**(-5), 51), np.linspace(-10**(-5), 10**(-5), 51))
+for i in range(np.size(X)):
+    for j in range(np.size(X)):
+        roots[i, j] = fsolve(lambda z: X[i] + 2*Y[j] + z + e**(2*z) - 1, 0)
+
+X_, Y_ = np.meshgrid(np.linspace(-1, 1, 51), np.linspace(-1, 1, 51))
 Z_ = roots
 
 
