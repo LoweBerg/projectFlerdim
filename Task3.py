8f5fd2e6e0b7671b4ed44b13d500825b6d363b7c
LoweBerg <126024255+LoweBerg@users.noreply.github.com>
2025-10-23 14:44:30 +0200

    Flyttade in sista delen av task 3

--- Task3.py
+++ Task3.py
@@ -25,32 +25,29 @@ def P_2(x, y):
     return roots[Mid, Mid] + DDX*x + DDY*y + (DFXX*x**2 + 2*DFXY*x*y + DFYY*y**2)/2
 
 
-print(roots[Mid+1, Mid+1])
-print(roots[Mid+1, Mid])
-print(DDX)
-print(DFXY)
 
-
-#Surface plot
-
-X = np.linspace(-1, 1, 51)
-Y = np.linspace(-1, 1, 51)
-roots = np.zeros((51, 51))#loop that iterates over every possible combination on the lists X and Y
+PVZ_2 = np.zeros((51, 51))#loop that iterates over every possible combination on the lists X and Y
 
 for i in range(np.size(X)):
     for j in range(np.size(X)):
-        roots[i, j] = fsolve(lambda z: X[i] + 2*Y[j] + z + e**(2*z) - 1, 0)
+        PVZ_2[i, j] = P_2(X[i], Y[j])
 
-X_, Y_ = np.meshgrid(np.linspace(-1, 1, 51), np.linspace(-1, 1, 51))
+# surface plot of Z and Taylor fcn
+
+X_, Y_ = np.meshgrid(np.linspace(-10**(-5), 10**(-5), 51), np.linspace(-10**(-5), 10**(-5), 51))
 Z_ = roots
 
-
-ax = plt.figure().add_subplot(projection='3d')
-ax.plot_surface(X_, Y_, P_2(X_, Y_), color = 'pink', alpha = 0.7)            # plotting taylor
-ax.plot_surface(X_, Y_, Z_, color= 'orange', alpha = 0.7)
-plt.xlabel('x')
-plt.ylabel('y')
+ax = plt.figure().add_subplot(projection='3d', xlabel = 'x', ylabel = 'y', zlabel = 'z')
+ax.plot_surface(X_, Y_, P_2(X_, Y_), color = 'purple', lw = 0.4, alpha = 0.7, edgecolors = 'purple')            # plotting taylor
+ax.plot_surface(X_, Y_, Z_, color= 'blue', lw = 0.4, alpha = 0.7, edgecolors = 'b')                    # plotting Z
+plt.title('Taylor fcn (purple) & Z fcn (blue)')
 plt.show()
 
+# calculating & plotting absolute error
+err = abs(Z_ - PVZ_2)
 
-
+ax = plt.figure().add_subplot(projection='3d', xlabel = 'x', ylabel = 'y', zlabel = 'z')
+plt.contourf(X_, Y_, err, levels = 150, cmap = 'inferno')
+plt.title('Absolute error')
+plt.colorbar()
+plt.show()
