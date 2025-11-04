a37d8495d7cd40b3102e6252f9605dee3a6d3bf3
Lowe Berg <loweberg04@gmail.com>
2025-11-04 15:37:37 +0100

    Added comments

--- Task3.py
+++ Task3.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
-from math import e, floor, sqrt
+from math import e, floor
 
 X = np.linspace(-10**-5, 10**-5, 51)
 Y = np.linspace(-10**-5, 10**-5, 51)
-roots = np.zeros((51, 51))#loop that iterates over every possible combination on the lists X and Y
+roots = np.zeros((51, 51))
 
+# loop that iterates over every possible combination on the lists X and Y
 for i in range(np.size(X)):
     for j in range(np.size(X)):
         roots[i, j] = fsolve(lambda z: X[i] + 2*Y[j] + z + e**(2*z) - 1, 0)
@@ -16,7 +17,8 @@ H = (2*10**-5)/50
 
 # coefficients
 
-'''
+# old bad derivatives
+'''   
 DDX = (roots[Mid+1, Mid]-roots[Mid, Mid])/H
 DDY = (roots[Mid, Mid+1]-roots[Mid, Mid])/H
 DFXX = (roots[Mid+2, Mid] - 2*roots[Mid+1, Mid] + roots[Mid, Mid])/(H**2)
@@ -24,7 +26,7 @@ DFXY = (roots[Mid+1, Mid+1]-roots[Mid, Mid+1]-roots[Mid+1, Mid]-roots[Mid, Mid])
 DFYY = (roots[Mid, Mid+2] - 2*roots[Mid, Mid+1] + roots[Mid, Mid])/(H**2)
 '''
 
-# Centered derivatives
+# Centered derivatives (nice)
 DDX = (roots[mid + 1, mid] - roots[mid - 1, mid]) / (2 * H)
 DDY = (roots[mid, mid + 1] - roots[mid, mid - 1]) / (2 * H)
 DFXX = (roots[mid + 2, mid] - 2 * roots[mid, mid] + roots[mid - 2, mid]) / (4 * H ** 2)
@@ -52,10 +54,10 @@ for i in range(np.size(X)):
     for j in range(np.size(X)):
         PVZ_2[i, j] = P_2(X[i], Y[j])
 
-#Surface plot of Z(x, y)
 
+# Surface plot of Z(x, y)
 X_, Y_ = np.meshgrid(np.linspace(-5, 5, 101), np.linspace(-5, 5, 101))
-Z_ = np.matrix_transpose(roots)
+Z_ = np.matrix_transpose(roots)  # Slap a transpose on that bad boy
 
 ax = plt.figure().add_subplot(projection='3d')
 ax.plot_surface(X_, Y_, Z_)
@@ -64,7 +66,6 @@ plt.xlabel('X')
 plt.ylabel('Y')
 
 # surface plot of Z and Taylor fcn
-
 ax = plt.figure().add_subplot(projection='3d', xlabel='x', ylabel='y', zlabel='z')
 ax.plot_surface(X_, Y_, P_2(X_, Y_), color='purple', lw=0.4, alpha=0.7, edgecolors='purple')            # plotting taylor
 ax.plot_surface(X_, Y_, Z_, color='blue', lw=0.4, alpha=0.7, edgecolors='b')                    # plotting Z
